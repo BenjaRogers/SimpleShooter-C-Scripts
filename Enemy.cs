bd3e327f9d5d36fb79a5cf01058def9f66325b57
teotz <64092117+teotz@users.noreply.github.com>
2020-10-04 17:20:15 -0400

    Add files via upload

---
+++ Enemy.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Enemy : MonoBehaviour
+{
+
+    
+    public float speed;
+    private Transform playerPos;
+    private Player player;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        playerPos = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, playerPos.position, speed * Time.deltaTime);
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            player.health--;
+            Debug.Log(player.health);
+            Destroy(gameObject);
+        }
+
+        if (other.CompareTag("Projectile"))
+        {
+            Destroy(gameObject);
+            Destroy(other.gameObject);
+        }
+    }
+}
---
+++ Player.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour
+{
+    public float speed;
+    public int health = 10;
+    private Rigidbody2D rb;
+    private Vector2 moveVelocity;
+    public Text healthDisplay;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        healthDisplay.text = "Health: " + health;
+        if (health <= 0)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+        Vector2 moveInput = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        moveVelocity = moveInput.normalized * speed;
+    }
+
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + moveVelocity * Time.fixedDeltaTime);
+    }
+}
